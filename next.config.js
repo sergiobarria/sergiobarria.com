74c22ced571817b73623b2ee04c32a5fa5b3e708
Sergio Barria <sergio.barria9210@gmail.com>
2021-08-23 16:01:10 -0500

    Remove unused files, add next/analyze

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   future: {
     strictPostcssConfiguration: true,
   },
@@ -13,4 +17,4 @@ module.exports = {
 
     return config;
   },
-};
+});
