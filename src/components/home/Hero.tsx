607f0cb0e4beef9f72f4131aa81a1e26935d2ff2
Sergio Barria <sergio.barria9210@gmail.com>
2022-01-03 22:20:04 -0500

    add hero section

---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <div className="w-10/12 mt-8">
+      <h1 className="mb-3 heading-1">Hi, I&apos;m Sergio</h1>
+      <p className="text-lg text-gray-400">Full Stack Web & Mobile Developer</p>
+      <p className="text-xl">
+        Welcome to my small piece of the internet, where I write and share about
+        web & mobile development topics.
+      </p>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import Hero from '@/components/home/Hero'
+
 export default function HomePage() {
-  return <div>Hello World, from the homepage</div>
+  return <Hero />
 }
 
 // import { getFeaturedProjects, getFeaturedPosts } from '@/lib/graphcms';
