0e88e7ea78a73cb69c08bdd75357d2dedd98a96f
Sergio Barria <sergio.barria9210@gmail.com>
2022-02-20 14:42:20 -0500

    refactor(gtag-favicon): refactor google tag and favicon

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,6 +1,8 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = url => {
-  window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+export const pageview = (url) => {
+  window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 import { DefaultSeo } from 'next-seo';
 import { ThemeProvider } from 'next-themes';
 
@@ -15,7 +16,7 @@ import Layout from '@/components/layout-main/Layout';
 import SEO from '../../next-seo.config';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const routerHook = useRouter();
+  const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url: any) => {
@@ -23,23 +24,44 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     };
     // When component is mounted, subscribe to router changes
     // and log those page views
-    routerHook.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteChange);
 
     // If component is unmounted, unsubscribe
     // from the event with the `off` method
     return () => {
-      routerHook.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteChange);
     };
-  }, [routerHook.events]);
+  }, [router.events]);
 
   return (
     <>
       <DefaultSeo {...SEO} />
+
       <ThemeProvider enableSystem={true} attribute='class'>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
+
+      {/* Google Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id='google-tag'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+        }}
+      />
     </>
   );
 }
